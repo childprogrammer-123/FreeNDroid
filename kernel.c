f9ee4a0745e255cde6c6454b89d4fb3ec7d8759c
childprogrammer-123 <yigitmehmetcintay2@gmail.com>
2026-03-14 20:19:07 +0300

    Update kernel.c
    The kernel.c file has been updated for the new FreeNDroid v0.2.

--- kernel.c
+++ kernel.c
@@ -1,5 +1,5 @@
 #define UART_BASE 0x09000000
-#define UARTFR    0x09000018
+#define UARTFR    0x09000018                                                    
 #define RXFE      0x10
 
 volatile unsigned int *uart    = (volatile unsigned int *)UART_BASE;
@@ -9,15 +9,23 @@ void uart_putc(char c) {
     *uart = c;
 }
 
-void uart_print(const char *str) {                                                
-  while (*str) {
-        uart_putc(*str++);
-    }
+void uart_print(const char *str) {                                                  
+   while (*str) {
+        uart_putc(*str++);                                                          
+   }
 }
-
+                                                                                
 char uart_getc() {
     while (*uartfr & RXFE) {}
     return (char)(*uart);
+}
+
+int str_eq(const char *a, const char *b) {
+    while (*a && *b) {
+        if (*a != *b) return 0;
+        a++; b++;
+    }
+    return *a == *b;
 }
 
 void uart_readline(char *buf, int max) {
@@ -29,6 +37,17 @@ void uart_readline(char *buf, int max) {
             uart_putc('\n');
             break;
         }
+        if (c == 127 || c == '\b') {
+            if (i > 0) {
+                i--;
+                uart_putc('\b');
+                uart_putc(' ');
+                uart_putc('\b');
+            } else {
+                uart_putc('\a');
+            }
+            continue;
+        }
         uart_putc(c);
         buf[i++] = c;
     }
@@ -36,11 +55,14 @@ void uart_readline(char *buf, int max) {
 }
 
 void kernel_main() {
-    uart_print("FreeNDroid v0.1 - Booting...\n");
+    uart_print("FreeNDroid v0.2 - Booting...\n");
 
     char buf[128];
     while (1) {
         uart_print("> ");
         uart_readline(buf, 128);
+        if (str_eq(buf, "help")) {
+            uart_print("This isn't a full OS. The developer will add something like bash command line for here. There are no commands on the command line right now.\n");
+        }
     }
 }
